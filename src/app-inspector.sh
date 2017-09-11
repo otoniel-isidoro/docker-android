c490e811b4a659c5a5a99a6701f1b6f651bd2df2
Otoniel <otoniel.isidoro@gmail.com>
2017-09-10 22:47:43 -0300

    adicionado configurações para parar serviços

--- src/app-inspector.sh
+++ src/app-inspector.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done'
+sleep 10
 adb install -r /root/src/apk/app-debug.apk
 adb install -r /root/src/apk/app-debug-androidTest.apk
-adb kill-server
-adb devices
-adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done'
 app-inspector -u $(adb devices | grep -o '\b\([a-f0-9]\|emulator-5554\)\+\b') --verbose
